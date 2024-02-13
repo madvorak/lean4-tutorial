afbd119cf422c2a47960c9803a3ea122b28f8693
madvorak <martin.dvorak@matfyz.cz>
2024-02-13 15:06:06 +0100

    after class

--- ISTA/Class3.lean
+++ ISTA/Class3.lean
@@ -1,6 +1,7 @@
 import Std.Tactic.Ext
 import Mathlib.Tactic.Have
 import Mathlib.Tactic.Lemma
+import Mathlib.Tactic.Convert
 
 variable {T : Type}
 
@@ -13,11 +14,15 @@ def cat : List T → List T → List T
 
 theorem cat_assoc (x y z : List T) :
     cat (cat x y) z = cat x (cat y z) := by
-  sorry
+  induction x with
+  | nil => rfl
+  | cons a l ih =>
+    dsimp only [cat]
+    rw [ih]
 
 theorem cat_nil (x : List T) :
     cat x [] = x := by
-  sorry
+  induction x <;> simp_all [cat]
 
 
 -- # Reversing lists
@@ -32,5 +37,20 @@ private def r : List T → List T → List T
 
 def rf (x : List T) : List T := r x []
 
+private lemma rev_eq_r (x y : List T) :
+    r x y = cat (rev x) y := by
+  revert y
+  induction x with
+  | nil =>
+    intro
+    rfl
+  | cons a l ih =>
+    intro k
+    dsimp only [r, rev]
+    rw [ih (a :: k), cat_assoc]
+    rfl
+
 theorem rev_eq_rf : @rev T = rf := by
-  sorry
+  ext x
+  unfold rf
+  rw [rev_eq_r, cat_nil]
